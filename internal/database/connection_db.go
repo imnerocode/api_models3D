bb7439ea09dc3bb3132cb44f9843173583b680be
imnerocode <jesalemalo@icloud.com>
2024-11-23 12:06:06 -0500

    feat(connection_db.go): Add function to retrieve connection from the client mongo.
    refa(NewConnectionDB): Now this function only initialize the structure.

--- internal/database/connection_db.go
+++ internal/database/connection_db.go
@@ -1,27 +1,52 @@
 package database
 
 import (
+	"context"
+	"fmt"
 	"os"
+	"time"
 
+	"github.com/imnerocode/apis/api_models3D/internal/config"
 	"github.com/joho/godotenv"
 	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
 type ConnectionDB interface {
-	NewConnectionDB(db *mongo.Client) *ConnectionDBImpl
 	GetConnectionDB() (*mongo.Client, error)
 	LoadEnv(pathToEnv string) string
 }
 
 type ConnectionDBImpl struct {
-	DB *mongo.Client
+	Client *mongo.Client
 }
 
-func NewConnectionDB(db *mongo.Client) *ConnectionDBImpl {
-	return &ConnectionDBImpl{DB: db}
+func NewConnectionDB() *ConnectionDBImpl {
+	return &ConnectionDBImpl{}
 }
 
-func (conn *ConnectionDBImpl) GetConnectionDB() *mongo.Client {
+func (conn *ConnectionDBImpl) GetConnectionDB() (*mongo.Client, error) {
+	var pathToEnv string
+	var keyEnv string
+
+	pathToEnv = config.PathToEnv
+	keyEnv = config.EnvMongoURI
+
+	uri := conn.LoadEnv(pathToEnv, keyEnv)
+
+	if uri == "" {
+		return nil, fmt.Errorf("failed to load environment variable %s", keyEnv)
+	}
+
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+
+	client, err := mongo.Connect(ctx, options.Client().ApplyURI(uri))
+	if err != nil {
+		return nil, fmt.Errorf("failed to connect to MongoDB: %w", err)
+	}
+	conn.Client = client
+	return client, nil
 
 }
 
@@ -33,7 +58,7 @@ func (conn *ConnectionDBImpl) LoadEnv(pathToEnv, keyEnv string) string {
 	varEnv, exists := os.LookupEnv(keyEnv)
 
 	if !exists {
-		return " "
+		return ""
 	}
 	return varEnv
 }
