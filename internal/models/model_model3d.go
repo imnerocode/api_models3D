cafc2a4a994a62287f06c1baebdd3801d29b9761
imnerocode <jesalemalo@icloud.com>
2024-11-23 12:26:09 -0500

    feat(model_model3d.go):
    Add scheme Model3D wich have the basic structure to handler models on the api

--- internal/models/model_model3d.go
+++ internal/models/model_model3d.go
@@ -1,1 +1,12 @@
 package models
+
+import "time"
+
+type Model3D struct {
+	ID        string     `bson:"_id,omitempty"`
+	Name      string     `bson:"name"`
+	Format    string     `bson:"format"`
+	Size      int64      `bson:"size"`
+	CreatedAt time.Time  `bson:"createdAt"`
+	DeletedAt *time.Time `bson:"deletedAt,omitempty"`
+}
