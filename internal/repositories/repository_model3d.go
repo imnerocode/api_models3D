499c94f438c4d96d07d634886e8dd8238167e618
imnerocode <jesalemalo@icloud.com>
2024-11-24 19:14:59 -0500

    feat(repository_model3d.go):
    Add functions to create, get and delete model3D

--- internal/repositories/repository_model3d.go
+++ internal/repositories/repository_model3d.go
@@ -1,7 +1,13 @@
 package repositories
 
 import (
+	"context"
+	"time"
+
+	"github.com/imnerocode/apis/api_models3D/internal/config"
 	"github.com/imnerocode/apis/api_models3D/internal/models"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
 	"go.mongodb.org/mongo-driver/mongo"
 )
 
@@ -14,6 +20,57 @@ func NewModel3DRepository(client *mongo.Client) *Model3DRepository {
 }
 
 func (r *Model3DRepository) PostModel3D(model3d *models.Model3D) (string, error) {
-	var databaseName string
-	var collectionName string
+	db := r.Client.Database(config.DatabaseName)
+	collection := db.Collection(config.CollectionName)
+
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	result, err := collection.InsertOne(ctx, model3d)
+	if err != nil {
+		return "", err
+	}
+	id := result.InsertedID.(primitive.ObjectID)
+	return id.Hex(), nil
+}
+
+func (r *Model3DRepository) GetModel3D(id string) (*models.Model3D, error) {
+	var model3D *models.Model3D
+	db := r.Client.Database(config.DatabaseName)
+	collection := db.Collection(config.CollectionName)
+
+	objectID, err := primitive.ObjectIDFromHex(id)
+	if err != nil {
+		return nil, err
+	}
+
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	if err := collection.FindOne(ctx, bson.M{"_id": objectID}).Decode(&model3D); err != nil {
+		return nil, err
+	}
+	return model3D, nil
+}
+
+func (r *Model3DRepository) DeleteModel3D(id string) (bool, error) {
+	var deletedModel3D *models.Model3D
+	db := r.Client.Database(config.DatabaseName)
+	collection := db.Collection(config.CollectionName)
+
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+	objectID, err := primitive.ObjectIDFromHex(id)
+	if err != nil {
+		return false, err
+	}
+
+	resultErr := collection.FindOneAndDelete(ctx, bson.M{"_id": objectID}).Decode(&deletedModel3D)
+
+	if resultErr != nil {
+		return false, nil
+	}
+
+	return true, nil
+
 }
