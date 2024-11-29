cac9861bf55588bbeba56465ce72413b7392bc1c
imnerocode <jesalemalo@icloud.com>
2024-11-29 14:03:03 -0500

    feat(model_model3d.go):
    Add json tags to structure

--- internal/models/model_model3d.go
+++ internal/models/model_model3d.go
@@ -3,11 +3,11 @@ package models
 import "time"
 
 type Model3D struct {
-	ID        string     `bson:"_id,omitempty"`
-	Name      string     `bson:"name"`
-	Format    string     `bson:"format"`
-	Size      int64      `bson:"size"`
-	CreatedAt time.Time  `bson:"createdAt"`
-	DeletedAt *time.Time `bson:"deletedAt,omitempty"`
-	Tags      []string   `bson:"tags"`
+	ID        string     `bson:"_id,omitempty" json:"_id"`
+	Name      string     `bson:"name" json:"name"`
+	Format    string     `bson:"format" json:"format"`
+	Size      int64      `bson:"size" json:"size"`
+	CreatedAt time.Time  `bson:"createdAt" json:"createdAt"`
+	DeletedAt *time.Time `bson:"deletedAt,omitempty" json:"deletedAt,omitempty"`
+	Tags      []string   `bson:"tags" json:"tags"`
 }
